abab144e21d6aadf1a925c4f56f88fd71f4a169a
Keirays <dangchihieu2512@gmail.com>
2026-09-24 01:50:58 +0700

    Buoi02 Bai02 gioi thieu ban than

---
+++ lib/src/main/java/Bai02_GioiThieuBanThan.kt
@@ -0,0 +1,9 @@
+fun main()
+{
+    val hoTen: String = "Dang Chi Hieu"
+    val namSinh: Int = 2002
+    val namHienTai: Int = 2026
+    val tuoi: Int = namHienTai - namSinh
+
+    println("Xin chao minh ten la: $hoTen, minh $tuoi Tuoi")
+}
